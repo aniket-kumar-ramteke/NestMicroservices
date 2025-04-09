3fb442fbd05b5a64793de67d5eeda52178b11d1d
aniket-neosoft <aniket.ramteke@neosoftmail.com>
2025-04-09 15:34:14 +0530

    feat: applied containerizarion for reservations service using Dockerfile and docker-compose.yaml in root

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateReservationDto {
+    @IsDate()
+    @Type(() => Date)
     start: Date;
+
+    @IsDate()
+    @Type(() => Date)
     end: Date;
+
+    @IsString()
+    @IsNotEmpty()
     placeId: string;
+
+    @IsString()
+    @IsNotEmpty()
     invoiceId: string;
 }
